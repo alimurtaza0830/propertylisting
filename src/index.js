538e3715bb0a492ba7a309ccbe05d448766c1ec0
Ali Murtaza <khattaksa345@gmail.com>
2020-01-11 22:44:22 +0100

    updated dependencies

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
-import "bootswatch/dist/litera/bootstrap.min.css";
+import "bootswatch/dist/slate/bootstrap.min.css";
 import App from './App';
 
 ReactDOM.render(
